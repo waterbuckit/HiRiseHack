d42d5ddd597f62588298dcddd223719306ed19df
unironically <lbvac230@gmail.com>
2019-12-07 09:23:25 +0000

    based

--- extractirb.py
+++ extractirb.py
@@ -1,12 +1,6 @@
 import cv2
 import sys
-
 print(sys.argv[1])
-
 src = cv2.imread(sys.argv[1], cv2.IMREAD_UNCHANGED)
-
-
 red_channel = src[:,:,2]
-
 cv2.imwrite("./tmp/redchannel.jpg", red_channel)
-
