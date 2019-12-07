3a43f53c93de6ef0fb67e65f0d477bda006f8182
waterbucket <adamroyal232@yahoo.com>
2019-12-07 12:15:22 +0000

    Fixed issue surrounding order

--- extractirb.py
+++ extractirb.py
@@ -2,7 +2,7 @@ import cv2
 import os
 import sys
 import imageio
-import PIL.Image
+from PIL import Image
 
 Image.MAX_IMAGE_PIXELS = None
 
@@ -20,15 +20,18 @@ red_channel = src[:,:,2]
 blue_channel = src[:,:,0]
 green_channel = src[:,:,1]
 
-cv2.imwrite("./tmp/red_channel.jpg", red_channel)
-cv2.imwrite("./tmp/green_channel.jpg", green_channel)
-cv2.imwrite("./tmp/blue_channel.jpg", blue_channel)
+cv2.imwrite("./tmp/2.png", red_channel)
+cv2.imwrite("./tmp/1.png", green_channel)
+cv2.imwrite("./tmp/0.png", blue_channel)
 
 images = []
-for file_name in os.listdir("./tmp"):
-    if file_name.endswith(".jpg"):
-        file_path = os.path.join("./tmp", file_name)
-        images.append(imageio.imread(file_path))
+for i in range(3):
+    images.append(imageio.imread("./tmp/%s.png" %(str(i)))) 
+
+#for file_name in os.listdir("./tmp"):
+#    if file_name.endswith(".jpg"):
+#        file_path = os.path.join("./tmp", file_name)
+#        images.append(imageio.imread(file_path))
 
 fileName = sys.argv[1].split("/", 1)[1].split(".",1)[0] + ".gif"
 imageio.mimsave("./results/%s" % (fileName), images)
