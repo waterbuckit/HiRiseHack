8d402e2f7e69a1b4707173ef3f8d7190650b32dd
unironically <lbvac230@gmail.com>
2019-12-07 10:41:32 +0000

    Altered getirb to take an image URL.

--- getirb.py
+++ getirb.py
@@ -4,6 +4,12 @@ from io import BytesIO
 import requests
 import os
 
+root = sys.argv[1]
+response = requests.get(root)
+img = Image.open(BytesIO(response.content))
+img.save("img/" + a['href'][:-1] + ".tiff")
+
+"""
 root = "https://hirise-pds.lpl.arizona.edu/PDS/EXTRAS/RDR/ESP/ORB_060100_060199/"
 page = requests.get(root)
 soup = BeautifulSoup(page.content, features="html5lib")
@@ -11,8 +17,13 @@ soup = BeautifulSoup(page.content, features="html5lib")
 if not os.path.exists('img'):
 	os.mkdir("img")
 
+# _IRB.NOMAP.JP2   
+#_IRB.NOMAP.browse.jpg
+
 for a in soup.find_all('a', href=True)[1:]:
-	url = root + a['href'] + a['href'][:-1] + "_IRB.NOMAP.browse.jpg"
+	url = root + a['href'] + a['href'][:-1] + "_IRB.NOMAP.JP2"
+	print(url)
 	response = requests.get(url)
 	img = Image.open(BytesIO(response.content))
-	img.save("img/" + a['href'][:-1] + ".png")
+	img.save("img/" + a['href'][:-1] + ".tiff")
+"""
